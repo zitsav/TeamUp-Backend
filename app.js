7009a3e5793fd86d0a179d5666f23c41f372bc28
Utsav Tomar <95949906+zitsav@users.noreply.github.com>
2024-06-12 10:55:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const workspaceRoutes = require('./routes/workspace')
 const workspaceMemberRoutes = require('./routes/workspaceMember')
 const fileUpload = require('express-fileupload')
 
-const port = 3000 || process.env.PORT;
+const port = 5000 || process.env.PORT;
 
 app.use(express.static('./public'))
 app.use(express.json());
